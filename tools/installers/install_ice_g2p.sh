7afacd8e76602a2974c2c82c2887d8467ee1061b
Gunnar Thor <ornolfsson@gmail.com>
2022-02-23 10:25:37 +0000

    Integrate icelandic g2p

--- tools/installers/install_ice_g2p.sh
+++ tools/installers/install_ice_g2p.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-
 if [ $# != 0 ]; then
     echo "Usage: $0"
     exit 1;
@@ -9,11 +8,13 @@ fi
 
 # Install ice-g2p
 if [ ! -e ice-g2p.done ]; then
+    rm -rf ice-g2p
     . activate_python.sh
-    pip install ice-g2p
+    git clone https://github.com/G-Thor/ice-g2p.git
     (
         set -euo pipefail
-        fetch-models
+        pip install ./ice-g2p/
+
     )
     touch ice-g2p.done
 else
