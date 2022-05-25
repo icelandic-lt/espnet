11b0edd99829c9b71c50be0864781f83f06a7e61
Gunnar Thor <ornolfsson@gmail.com>
2022-05-25 14:01:12 +0000

    add conf files and implement data scripts

--- egs2/talromur2/tts1/train_multi_speaker_VITS.sh
+++ egs2/talromur2/tts1/train_multi_speaker_VITS.sh
@@ -17,20 +17,29 @@ test_sets="${valid_set} ${eval_set}"
     --local_data_opts "all"
 
 # # Since ice-g2p phonetization is very slow, dump phonemized text at first
-./local/phonetize.sh all
+./phonetize.sh all
 
 # Run from stage 2
 ./run.sh \
-    --train_set "${train_set}_phn" \
-    --valid_set "${valid_set}_phn" \
-    --test_sets "${valid_set}_phn ${eval_set}_phn" \
-    --srctexts "data/${train_set}_phn/text" \
+    --train_set train_all_phn \
+    --valid_set dev_all_phn \
+    --test_sets "dev_all_phn eval1_all_phn" \
+    --srctexts "data/train_all_phn/text" \
     --expdir "$expdir" \
     --stage 2 \
     --g2p none \
     --cleaner none \
     --use_xvector true \
+    --min_wav_duration 0.38 \
     --ngpu 4 \
-    --expdir "$expdir" \
-    --train_config ./conf/tuning/train_xvector_tacotron2.yaml \
-    --inference_model dev.total_count.ave.pth
+    --fs 22050 \
+    --n_fft 1024 \
+    --n_shift 256 \
+    --dumpdir dump/all/22k \
+    --expdir exp/all/22k \
+    --win_length null \
+    --tts_task gan_tts \
+    --feats_extract linear_spectrogram \
+    --feats_normalize none \
+    --train_config ./conf/tuning/train_xvector_vits.yaml \
+    --inference_model train.total_count.ave.pth
